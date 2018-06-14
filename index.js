d08c00a9958ba717594fd4b6b55a9e69af53af89
Michael Schwartz <mschwartz@robospeak>
2018-06-14 11:32:42 -0700

    working daemon!

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-const say = require("say");
+process.env.DEBUG = "speak";
 
-say.speak("Hello");
+const debug = require("debug")("speak"),
+  say = require("say"),
+  mqtt = require("mqtt"),
+  uri = `mqtt://${process.env.MQTT_HOST}`,
+  client = mqtt.connect(uri);
+
+const speak = async text => {
+  return new Promise((resolve, reject) => {
+    say.speak(text, null, null, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+client.on("connect", () => {
+  debug("connected");
+  client.subscribe("say");
+});
+
+client.on("message", async (topic, message) => {
+  debug("topic", topic, "message", message.toString());
+  await speak(message.toString());
+});
+
+client.on("error", e => {
+  debug("error", e);
+});
+
+//const main = async () => {
+//  await speak("The garage door is open");
+//  await speak("The garage door is closed");
+//};
+
+//main();
