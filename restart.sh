0f72fa7324ed33bf0eb1ca33335f234f408b233e
Michael Schwartz <mykesx@gmail.com>
2018-12-11 15:04:20 -0800

    user forever instead of pm2

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd /home/mschwartz/github/robodomo/speak
-/home/mschwartz/github/robodomo/speak/node_modules/.bin/pm2 restart ./index.js
+/home/mschwartz/github/robodomo/speak/node_modules/.bin/forever restart ./index.js
 
